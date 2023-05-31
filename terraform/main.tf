81b64409bf8fd1c99601cc40f4e1a5a45b636137
spartzames <spartzames@gmail.com>
2023-05-31 23:35:06 +0900

    add provider

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,8 @@
+provider "aws" {
+  profile = "default"
+  region = var.ct_home_region
+}
+
 resource "aws_s3_account_public_access_block" "block_all_public" {
   block_public_acls   = true
   block_public_policy = true
