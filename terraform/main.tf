67b1c5df32a20b641ded57549b14d2c23301acea
spartzames <spartzames@gmail.com>
2023-05-31 23:36:45 +0900

    add variable

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,6 +3,15 @@ provider "aws" {
   region = var.ct_home_region
 }
 
+variable "ct_home_region" {
+  description = "The region from which this module will be executed. This MUST be the same region as Control Tower is deployed."
+  type        = string
+  validation {
+    condition     = can(regex("(us(-gov)?|ap|ca|cn|eu|sa|me|af)-(central|(north|south)?(east|west)?)-\\d", var.ct_home_region))
+    error_message = "Variable var: region is not valid."
+  }
+}
+
 resource "aws_s3_account_public_access_block" "block_all_public" {
   block_public_acls   = true
   block_public_policy = true
